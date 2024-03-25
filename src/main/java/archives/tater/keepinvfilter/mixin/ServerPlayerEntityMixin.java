ae65edab9d73cd81c1497255011a34587124998d
JosiahFu <josiahfu@gmail.com>
2024-03-25 13:59:51 -0700

    Cleanup mixin

--- src/main/java/archives/tater/keepinvfilter/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/archives/tater/keepinvfilter/mixin/ServerPlayerEntityMixin.java
@@ -10,17 +10,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
-    @Shadow public abstract void onLanding();
-
-    @Shadow public abstract void onTeleportationDone();
-
     public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
     }
